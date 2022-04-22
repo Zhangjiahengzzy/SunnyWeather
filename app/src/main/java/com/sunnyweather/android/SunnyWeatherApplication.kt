6e10b8c6b9f6a4e257a13f0ea5537a66d6d7411d
Zzy <2996416632@qq.com>
2022-04-22 19:00:28 +0800

    实现搜索全球城市数据功能。

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,19 @@
+package com.sunnyweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+
+    companion object{
+        const val TOKEN = "5ARpimZ1bR1v8x4i"
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
